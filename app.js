fcee71d4cb4bd95acbfed5bd6a3c99e167b2e987
Ulugbek-Muqimjonov <muqimjonovulugbekgidgub@gmail.com>
2024-08-15 22:43:54 +0100

    sdsd

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require("express");
 
 const app = express();
-
+app.use(express.json());
+app.get("/", (req, res) => {
+  // res.send("Hello Ulug'bek ")
+  res.status(200).send("ULugbek");
+});
+app.post("/", (req, res) => {
+  const { firstName, lastName } = req.body;
+  res.send(`HIS fullName - ${firstName} ${lastName}`);
+});
+app.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(id);
+});
+app.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    id,
+    body,
+  });
+});
 const PORT = 8080;
 app.listen(PORT, () => console.log(`Listen on - http://localhost:${PORT}`));
